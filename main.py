19d3155cf7f899f45fb798df08a6d71f16b1834c
hoigoysohio <njuli@yahoo.com>
2024-12-17 17:28:42 +0000

    done

---
+++ main.py
@@ -0,0 +1,9 @@
+num_1 = 10
+num_2 = 15
+
+print("And:" ,num_1 & num_2)
+print("Or:" ,num_1 | num_2)
+print("Not:" ,~num_2)
+print("XOR:" ,num_1 ^ num_2)
+print("Leftshift by one:" ,num_2 << 1)
+print("Rightshift by one:" ,num_1 >> 1)
